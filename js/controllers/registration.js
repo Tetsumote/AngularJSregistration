5143acc9fe3dd06891ecf57468bed510262105cd
tetsumote <unionproject88@gmail.com>
2017-05-06 05:15:08 +0200

    com.8 AngularJS cleaning authentication code

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,25 +1,14 @@
 myApp.controller('RegistrationController',
-		['$scope','$firebase','$firebaseAuth',
-		 function($scope,$firebase,$firebaseAuth){
-			 
-		var ref = firebase.database().ref();
-		var auth = $firebaseAuth();
+		['$scope','Authentication',
+		 function($scope,Authentication){	
 	
 		$scope.login = function(){
-			$scope.message = "Welcome " + $scope.user.email;
-		};
+			Authentication.login($scope.user);
+		};//login
 	
 		$scope.register = function(){
+			Authentication.register($scope.user);
 			
-			auth.$createUserWithEmailAndPassword(
-			$scope.user.email,
-			$scope.user.password
-			).then(function(regUser){
-			$scope.message = "Hi " + $scope.user.firstname +
-			", Thanks for registering";	
-			}).catch(function(error){
-				$scope.message = error.message;
-			});//createUserWithEmailAndPassword
 		};	//register
 	
 }]);//Controller

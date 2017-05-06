e34f1ecdfa3d4b2a4ee6b651736fc5e994847a33
tetsumote <unionproject88@gmail.com>
2017-05-06 06:14:42 +0200

    com.9 AngularJS firebase another step.

--- js/services/authentication.js
+++ js/services/authentication.js
@@ -1,19 +1,44 @@
 myApp.factory('Authentication',
-['$rootScope','$firebaseAuth',
-function($rootScope,$firebaseAuth){
+['$rootScope','$location','$firebaseObject','$firebaseAuth',
+function($rootScope,$location,$firebaseObject,$firebaseAuth){
 	
 	var ref = firebase.database().ref();
 	var auth = $firebaseAuth();
 	
+	auth.$onAuthStateChanged(function(authUser){
+		if(authUser){
+			var userRef = ref.child('users').child(authUser.uid);
+			var userObj = $firebaseObject(userRef);
+			$rootScope.currentUser = userObj;
+		}else{
+			$rootScope.currentUser = '';
+		}
+	});
+	
 	return{
 		login: function(user){
-			$rootScope.message = "Welcome " + $rootScope.user.email;
+		auth.$signInWithEmailAndPassword(
+			user.email,
+			user.password
+			).then(function(user){
+			$location.path('/success')
+		}).catch(function(error){
+			$rootScope.message = error.message;
+		});//signInWithEmailAndPassword
 		},//login
 		register: function(user){
 			auth.$createUserWithEmailAndPassword(
 			user.email,
 			user.password
 			).then(function(regUser){
+			var regRef = ref.child('users')
+			.child(regUser.uid).set({
+				date: firebase.database.ServerValue.TIMESTAMP,
+				regUser: regUser.uid,
+				firstname: user.firstname,
+				lastname: user.lastname,
+				email: user.email
+			});//userinfo
 			$rootScope.message = "Hi " + user.firstname +
 			", Thanks for registering";	
 			}).catch(function(error){

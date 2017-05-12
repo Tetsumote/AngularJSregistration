9f7b50ba2fb8f7e2c97ef1088ed38ca481181bb6
Tetsumote <unionproject88@gmail.com>
2017-05-13 01:48:44 +0200

    problem with connection to database

--- js/controllers/meetings.js
+++ js/controllers/meetings.js
@@ -1,3 +1,22 @@
-myApp.controller('MeetingsController',['$scope',function($scope){
-	$scope.message = "Success!!!";
-}]);
+myApp.controller('MeetingsController',['$scope','$firebaseAuth','$firebaseObject','$firebaseArray',function($scope,$firebaseAuth){
+//connect to firebase and check auth
+	var ref = firebase.database().ref();
+	var auth = $firebaseAuth();
+	
+	auth.$onAuthStateChanged(function(authUser){
+		if(authUser){
+			
+			var meetingsRef = ref.child('users').child(authUser.uid).child('meetings');
+			var meetingsInfo = $firebaseArray(meetingsRef);
+			console.log(ref);
+			$scope.addMeeting = function(){
+				meetingsInfo.$add({
+					name: $scope.meetingname,
+					data: firebase.database.ServerValue.TIMESTAMP 
+				}).then(function(){
+					$scope.meetingname = '';
+				}); //promise
+			}; //addMeeting
+		}//authUser
+	});//onAuthStateChanged
+}]);// myApp controller  MeetingsController
